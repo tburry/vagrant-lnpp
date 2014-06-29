76fc1513383b5a6caf77f08b3a9f8c2f1e94945a
Todd Burry <todd@vanillaforums.com>
2014-06-29 12:15:11 -0400

    Add some customizations to our config.

--- puppet/manifests/provision/nginx/vhosts.pp
+++ puppet/manifests/provision/nginx/vhosts.pp
@@ -6,9 +6,26 @@ class provision::nginx::vhosts
   $sites_dir = $core::params::sites_dir
   $nginx_dir = "${core::params::templates_dir}/nginx"
 
-  file { "/etc/nginx/fastcgi.conf":
+  file { "error_pages.conf":
     ensure => file,
-    source => "${nginx_dir}/fastcgi.conf.erb"
+    path => "/etc/nginx/error_pages.conf",
+    source => "${nginx_dir}/error_pages.conf",
+    require => Package["nginx"]
+  }
+
+  file { "fastcgi.conf":
+    ensure => file,
+    path => "/etc/nginx/fastcgi.conf",
+    # source => template("${nginx_dir}/fastcgi.conf.erb"),
+    source => "${nginx_dir}/fastcgi.conf",
+    require => Package["nginx"]
+  }
+
+  file { "upstreams.conf":
+    ensure => file,
+    path => "/etc/nginx/conf.d/upstreams.conf",
+    source => "${nginx_dir}/upstreams.conf",
+    require => Package["nginx"]
   }
 
   nginx::vhost { "default":
@@ -24,11 +41,30 @@ class provision::nginx::vhosts
   }
 
   # Pre-defined setups
+  # nginx::vhost { "vanilla.dev":
+  #   root        => "${sites_dir}/vanilla",
+  #   index       => "index.php",
+  #   template   => "${nginx_dir}/vanilla.conf.erb"
+  # }
 
-  nginx::vhost { "vanilla.dev":
-    root     => "${sites_dir}/vanilla",
-    index    => "index.php",
-    template => "${nginx_dir}/vanilla.conf.erb"
+  define nginx::vhost::vanilla (
+    $host        = "$title",
+    $php_server  =  "php_fpm",
+    $root        =  "${core::params::sites_dir}/vanilla"
+  ) {
+    nginx::vhost { $host:
+      root        => $root,
+      index       => "index.php",
+      template    => "${provision::nginx::vhosts::nginx_dir}/vanilla.conf.erb"
+    }
+  }
+
+  nginx::vhost::vanilla { "vanilla.dev":
+    php_server  => "php_fpm"
+  }
+
+  nginx::vhost::vanilla { "vanilla.hhvm":
+    php_server  => "php_hhvm"
   }
 
   # nginx::vhost { "kirby":
